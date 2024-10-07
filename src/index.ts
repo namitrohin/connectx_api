be251193845a63b309bda4a04c7273e6e4512d4a
namit <support@rohininfotech.com>
2024-10-07 10:23:49 +0530

    initial change

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.use(compression(comp));
 
 app.get('/', (_req: Request, res: Response) => {
     // Send a response to the client
-    res.send('Hello, Connect X ,Under Development');
+    res.send('Hello, Connect X ,Devloped by RohinInfo tech');
 });
 app.use('api/v0', routes)
 routes
