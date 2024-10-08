3b74306ee9c1e548f936fd1d751f6ebf7828684f
Shivani <shivani@rohininfo.com>
2024-10-08 13:07:15 +0530

    nothing

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(cors());
 app.use(compression(comp));
-// Specify the port number for the server
+// Specify the port number for the server//
 
 app.get('/', (_req: Request, res: Response) => {
     // Send a response to the client
